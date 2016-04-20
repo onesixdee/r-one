644795ca929888735a886391ad042f0bbc44ceac
Diana Duong <dduong013@gmail.com>
2016-04-20 16:43:55 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-'use strict'
+// 'use strict'
 
 $(function() {
 
-	var firstName = $('#fname')
-	var lastName = $('#lname')
-	var company = $('#comany')
-	var title = $('#title')
-	var country = $('#country')
-	var state = $('#state')
-
+var submitForm = $('.submit-form')
 
 	$('#submit').click(function() {
 		console.log("submit button clicked")
-		firstName.val()
-		console.log(firstName)
 
+		var firstName = $("input[name='fname']").val()
+		var lastName = $("input[name='lname']").val()
+		var company = $("input[name='company']").val()
+		var title = $("input[name='title']").val()
+		var country = $("select[name='country']").val()
+		var state = $("select[name='states']").val()
+		var comments = $("textarea[name='comments']").val()
+
+		console.log('<p>Name:' + firstName + ' ' + lastName + '</p><br><p>Company and Title:' + company + ', ' + title + '</p><br><p>Country and State:' + country + ', ' + state)
+
+		submitForm.append('<p>Name:' + firstName + ' ' + lastName + '</p><br><p>Company and Title:' + company + ', ' + title + '</p><br><p>Country and State:' + country + ', ' + state)
 	})
-
 })
