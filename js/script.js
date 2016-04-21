a2e24470a730696dec9c2f86b7ea19359ec037b3
Diana Duong <dduong013@gmail.com>
2016-04-21 16:09:30 -0500

    submit click to append inputs

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-// 'use strict'
-
-$(function() {
-
-var submitForm = $('.submit-form')
-
-	$('#submit').click(function() {
-		console.log("submit button clicked")
-
-		var firstName = $("input[name='fname']").val()
-		var lastName = $("input[name='lname']").val()
-		var company = $("input[name='company']").val()
-		var title = $("input[name='title']").val()
-		var country = $("select[name='country']").val()
-		var state = $("select[name='states']").val()
-		var comments = $("textarea[name='comments']").val()
-
-		console.log('<p>Name:' + firstName + ' ' + lastName + '</p><br><p>Company and Title:' + company + ', ' + title + '</p><br><p>Country and State:' + country + ', ' + state)
-
-		submitForm.append('<p>Name:' + firstName + ' ' + lastName + '</p><br><p>Company and Title:' + company + ', ' + title + '</p><br><p>Country and State:' + country + ', ' + state)
-	})
-})
+  'use strict';
+ $(function() {
+   	 var submitForm = $('.submit-form');
+     var firstName = $("input[name='fname']").val();
+     var lastName = $("input[name='lname']").val();
+     var company = $("input[name='company']").val();
+     var title = $("input[name='title']").val();
+     var country = $("select[name='country']").val();
+     var state = $("select[name='states']").val();
+     var comments = $("textarea[name='comments']").val();
+   	$('#submit').click(function(event) {
+   	 event.preventDefault();
+    	submitForm.append('<p>First Name: ' + firstName + '</p><br><p>Last Name: ' + lastName + '</p><br><p>Company: ' + company + '</p><br><p>Title: ' + title + '</p><br><p>Country: ' + country + '</p><br><p>State: ' + state + '</p><br><p>Additional Comments: ' + comments + '</p>');
+ 	});
+ });
